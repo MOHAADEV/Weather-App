09d19c679df2d5c13bf850b0028fdb53d390d1d8
MOHAADEV <mmohammad.mdbf21@iste.edu.tr>
2025-01-18 19:31:17 +0300

    edit

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -9,23 +9,18 @@ class WeatherController extends Controller
 {
     public function showWeather(Request $request)
     {
-        // Get the API key from .env
         $apiKey = env('OPENWEATHERMAP_API_KEY');
 
-        // Get the location from the request (default to London)
         $location = $request->input('location', 'London');
 
-        // Fetch weather data from OpenWeatherMap API
         $response = Http::get("https://api.openweathermap.org/data/2.5/weather", [
             'q' => $location,
             'appid' => $apiKey,
-            'units' => 'metric', // Use 'imperial' for Fahrenheit
+            'units' => 'metric', 
         ]);
 
-        // Decode the JSON response
         $weatherData = $response->json();
 
-        // Pass the data to the view
         return view('weather', compact('weatherData'));
     }
 }
